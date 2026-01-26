973200f5e7fae9d5f0a219ceb091d9dbdf6f80d8
Marcelofury <marcelofury13579@gmail.com>
2026-01-26 18:03:52 +0300

    added toast function no alert()

--- auth/auth.js
+++ auth/auth.js
@@ -1,3 +1,4 @@
+Default user set up
 (function initDefaultUser(){
     if (!localStorage.getItem('registrationUsers')){
         const users = [
@@ -13,3 +14,18 @@
     }
 })();
 
+//Toast function ( No alert())
+function showToast(message, type){
+    const toast = document.createElement('div');
+    toast.className = `toast ${type}`;
+    toast.textContent = message;
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => toast.classList.add('show'),100);
+
+    setTimeout (() => {
+        toast.classList.remove('show');
+        setTimeout(() => toast.remove(), 500);
+    },3000);
+}
